0429524b4155571c333f431fd392406b8ea31289
AnanyaSVAB <anusvab@gmail.com>
2023-12-14 14:32:41 +0530

    Project initialised

---
+++ User.java
@@ -0,0 +1,22 @@
+package com;
+
+class User {
+	private int userId;
+	private String userName;
+
+	private String userEmail;
+	private String userPassword;
+	public int getUserId() {
+		return userId;
+	}
+	public void setterEmail(String userEmail) {
+		this.userEmail=userEmail;
+	}
+	public String getuserPassword() {
+		return userPassword;
+	}
+	public void setuserPasssword(String userPassword) {
+		this.userPassword=userPassword;
+	}
+}
+
